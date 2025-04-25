498e45374c80d816a8b14e48753f39a2dce35856
ajgonzalez59 <ajgonzalez5@espe.edu.ec>
2025-04-25 07:58:32 -0500

    Update PRUEBAXD.cpp

--- PRUEBAXD.cpp
+++ PRUEBAXD.cpp
@@ -18,7 +18,15 @@ return(a+b)
 
 int main()
 {
-    cout << "Hello world!" << endl;
+   float num1,num2,resultado;
+    char operacion ;
+
+    
+    
+    cout << "Ingrese el numero" << endl;
+    cin>> num1;
+    cout<<"ingrese el segundo numero"<<endl;
+    cin>>num2;
     return 0;
 }
 
