a1a0bf69790cdf204c6f41627a88056ae1090f46
aespinosa4 <amespinosa4@espe.edu.ec>
2025-04-25 08:46:19 -0500

    Update PRUEBAXD.cpp

--- PRUEBAXD.cpp
+++ PRUEBAXD.cpp
@@ -12,23 +12,25 @@ float multiplicar(float a,float b){
 return a*b;
 }
 float divicion(float a,float b){
-return a/b;
-   else{
-      cout<<"Error:no se puede divir entre cero"<<end1; 
-   } 
+   if (b!=0){
+    return a/b;
+   }else{
+      cout<<"Error:no se puede divir entre cero"<<endl;
+      return 0;
+   }
 }
 
 int main()
 {
    float num1,num2,resultado;
     char operacion ;
-    
+
     cout << "Ingrese el numero" << endl;
     cin>> num1;
    cout<<"ingrese un operador"<<endl;
    cin>> operacion;
     cout<<"ingrese el segundo numero"<<endl;
-    num2;
+    cin>>num2;
    switch (operacion){
    case'+':
       resultado=sumar(num1,num2);
@@ -40,7 +42,7 @@ int main()
       resultado=multiplicar(num1,num2);
       break;
    case'/':
-      resultado=dividir(num1,num2);
+      resultado=divicion(num1,num2);
       break;
       default:
          cout<<"operacion no valida"<<endl;
@@ -49,8 +51,3 @@ int main()
       cout<<"Resultado:"<< resultado <<endl;
     return 0;
 }
-
-
-
-
-
